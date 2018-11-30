fa4e2be9ddea61e766586d0f47e0fd24ec636fcc
Ozziiii <45219376+Ozziiii@users.noreply.github.com>
2018-11-29 23:48:43 -0800

    Add files via upload

---
+++ simpleboard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CE361Final;
+public class board
+{
+    private string[,] cboard;
+    public board()
+    {
+        cboard = new string[8, 8];//Size of chessboard
+   
+    }
+}
+
