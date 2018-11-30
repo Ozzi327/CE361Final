1f4ed31f2d65fce7e15a0cb333a2f898ae730690
jonathantran42 <45477883+jonathantran42@users.noreply.github.com>
2018-11-29 22:28:39 -0800

    Add files via upload

---
+++ rook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Jonathan Tran
+/// 819325761   
+/// not working code at all 
+/// have to see what will happen with the board class and how that will interact with
+/// the pieces
+/// </summary>
+namespace Rook
+{
+    public class Rook
+    {
+        
+         Rook = new char[board.x, board.y];//location of the rook
+         
+            
+            public Rook()
+            {
+                
+            }
+            /// <summary>
+            /// movement of the rook 
+            /// checks for validity of the move
+            /// </summary>
+             private void rkMOVE()
+             {
+             if (board.x <= 7 || board.y <= 7) //moves the rook --> can go straight in either x or y plane
+                 {
+                     //will only continue if the move is possible
+                     if ((board.x >= 0) && (board.x <= 7) && (board.y =< 7) && (board.y >= 0))
+                     {
+                         if (player.color[board.x, board.y] != player.color[board.x, board.y])
+                         {
+                             move = true;
+                             // cannot move to this place if there is another of your piece already there
+                             // would probably have to see if there are others in front of it as well though
+                         }
+                         else
+                             move = false; //cannot move to a place with a piece there
+                     }
+                     else
+                         move = false; //cannot move out of bounds
+                 }
+                 else
+                    move = false; //cannot move beyond the bounds of the rook's movement
+             }
+        
+    }
+}
---
+++ space.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/// <summary>
+/// jonathan tran
+/// 819325761
+/// this is the class of individual spaces
+/// </summary>
+public class space
+{
+    private bool occupied { get; set; }; //to see if there is a piece here
+    private int x { get; set; }; //x-axis of the piece 
+    private int y { get; set; }; //y-axis of the piece
+    //may have to add something for the color of the piece on the tile
+
+    public space()
+    {
+        this.occupied = false;
+        this.x = 0;
+        this.y = 0;
+    }
+    /// <summary>
+    /// EX constructor for the space
+    /// </summary>
+    /// <param name="X"></param>
+    /// <param name="Y"></param>
+    public space(int X, int Y,bool occ)
+    {
+        this.occupied = occ;
+        if (X > = 0 || X <= 7)
+        {
+            this.x = X;
+        }
+        if (Y > = 0 || Y <= 7)
+        {
+            this.y = Y;
+        }
+    }
+
+    public inSpace()
+    {
+        this.occupied = true;
+    }
+
+    public outSpace()
+    {
+        this.occupied = false;
+    }
+        
+}
