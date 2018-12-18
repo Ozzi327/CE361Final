ba1cfed2a8e9852aa471a40a9d29f49cdb9beb32
Dean4444 <45476041+Dean4444@users.noreply.github.com>
2018-12-18 15:09:30 -0800

    Add files via upload

--- Bishop.cs
+++ Bishop.cs
@@ -2,114 +2,122 @@
 
 public class Bishop : GamePiece, IComparable<Bishop>
 {
-	public Bishop(string PieceType, Thickness BishopPositionX , Thickness BishopPositionY, bool ColorofPiece = false): base(PieceType, BishopPositionX, BishopPositionY, false)
-	{
+    public Bishop(string PieceType, Thickness BishopPositionX, Thickness BishopPositionY, bool ColorofPiece = false) : base(PieceType, BishopPositionX, BishopPositionY, false)
+    {
         PieceType = "Bishop";
         PieceColor = ColorofPiece;
         PositionX = BishopPositionX;
         PositionY = BishopPositionY;
-	}
+    }
 
 
     private Thickness tempPositionX;
     private Thickness tempPositionY;
-}
 
-public override string Tostring()
-{
-    return $"The Team {Piececolor} Bishop has moved ({PositionX}, {PositionY}) spaces";
-}
 
-public override bool Equals(object obj) // checks if object is a King type
-{
-    if (obj is Bishop && obj != null)
+    public override string Tostring()
     {
-        Bishop piece = (Bishop)obj;
-        return PieceColor.Equals(piece.PieceColor) && PositionX.Equals(piece.PositionX) && PositionY.Equals(piece.PositionY);
+        return $"The Team {Piececolor} Bishop has moved ({PositionX}, {PositionY}) spaces";
     }
-    else
-    {
-        return false;
-    }
-}
 
-public override int GetHashCode()
-{
-    return base.GetHashCode();
-}
-
-public int CompareTo(Bishop otherPiece)
-{
-    if (PieceColor.CompareTo(otherPiece.PieceColor) == 0)
+    public override bool Equals(object obj) // checks if object is a King type
     {
-        if (PositionX.Left.CompareTo(otherPiece.PositionX.Left) == 0)
+        if (obj is Bishop && obj != null)
         {
-            return PositionY.Top.CompareTo(otherPiece.PositionY.Top); // gets difference of Y position of 2 pieces
+            Bishop piece = (Bishop)obj;
+            return PieceColor.Equals(piece.PieceColor) && PositionX.Left.Equals(piece.PositionX.Left) && PositionY.Top.Equals(piece.PositionY.Top);
         }
         else
         {
-            return PositionX.Left.CompareTo(otherPiece.PositionX.Left); // gets difference of X postion of 2 pieces
+            return false;
         }
     }
-    else
-    {
-        return PieceColor.CompareTo(otherPiece.PieceColor); // gets difference of color of 2 pieces
-    }
-}
 
-public bool validMove(Gamepiece b)
-{
-    if (((this.PositionX.Left >= 200) && (this.PositionX.Left <= 690)) && ((this.PositionY.TOp >= 70) && (this.PositionY.Top <= 560))) // checks if new move is within boundaries of board
+    public override int GetHashCode()
     {
-        if ((this.PositionX.Left == b.PositionY.Top) && (this.PositionX.Left == b.PositionY.Top)) // checks if two pieces are on the same space on the board
+        return base.GetHashCode();
+    }
+
+    public int CompareTo(Bishop otherPiece)
+    {
+        if (PieceColor.CompareTo(otherPiece.PieceColor) == 0)
         {
-            if (this.PieceColor != b.PieceColor) // checks if 2 pieces are of different color
+            if (PositionX.Left.CompareTo(otherPiece.PositionX.Left) == 0)
             {
-                return true; // valid move
+                return PositionY.Top.CompareTo(otherPiece.PositionY.Top); // gets difference of Y position of 2 pieces
             }
             else
             {
-                return false; // invalid move
+                return PositionX.Left.CompareTo(otherPiece.PositionX.Left); // gets difference of X postion of 2 pieces
             }
         }
         else
         {
-            return true; // valid move 
+            return PieceColor.CompareTo(otherPiece.PieceColor); // gets difference of color of 2 pieces
         }
     }
-    else
-    {
-        return false; // invalid move
-    }
-}
 
-public void removePiece(Bishop a, Bishop b)
-{
-    if ((b.PositionX.Left == a.PositionX.Left) && (b.PositionY.Top == a.PositionX.Left)) // checks if piece B has moved into piece A space on the board
+    public void Move(double inputX, double inputY, Thickness x, Thickness y, Image BS)
     {
-        if (b.PieceColor != a.PieceColor) // checks if piece A and piece B are the same color
+
+        if ((inputX == inputY) || (inputX == (inputY * -1)))// checks to make sure the move is diagnal of the piece otherwise wont move
         {
-            a.PieceColor = null; // Piece A color is deleted
+            x = BS.Margin;
+            x.Left = x.Left + inputX * 70;
+            y = BS.Margin;
+            y.Top = y.Top + inputY * 70;
+            BS.Margin = x;
+            BS.Margin = y;
+        }
+        else
+            Console.WriteLine("make x and y be the same number positive or negative dont matter");
+
+
+    }
+
+    public bool validMove(Gamepiece b)
+    {
+        if (((this.PositionX.Left >= 200) && (this.PositionX.Left <= 690)) && ((this.PositionY.Top >= 70) && (this.PositionY.Top <= 560))) // checks if new move is within boundaries of board
+        {
+            if ((this.PositionX.Left == b.PositionY.Top) && (this.PositionX.Left == b.PositionY.Top)) // checks if two pieces are on the same space on the board
+            {
+                if (this.PieceColor != b.PieceColor) // checks if 2 pieces are of different color
+                {
+                    return true; // valid move
+                }
+                else
+                {
+                    return false; // invalid move
+                }
+            }
+            else
+            {
+                return true; // valid move 
+            }
+        }
+        else
+        {
+            return false; // invalid move
         }
     }
 
-}
-
-public void Move(double inputX, double inputY, Thickness x, Thickness y, Image BS)
-{
-
-    if ((inputX == inputY) || (inputX == (inputY * -1)))// checks to make sure the move is diagnal of the piece otherwise wont move
+    public static void Moveset()
     {
-        x = BS.Margin;
-        x.Left = x.Left + inputX * 70;
-        y = BS.Margin;
-        y.Top = y.Top + inputY * 70;
-        BS.Margin = x;
-        BS.Margin = y;
+        this.PostionX = this.tempPositionX;
+        this.PositionY = this.tempPositionY;
     }
-    else
-        Console.WriteLine("make x and y be the same number positive or negative dont matter");
-    
+
+    public void removePiece(Bishop a, Bishop b)
+    {
+        if ((b.PositionX.Left == a.PositionX.Left) && (b.PositionY.Top == a.PositionX.Left)) // checks if piece B has moved into piece A space on the board
+        {
+            if (b.PieceColor != a.PieceColor) // checks if piece A and piece B are the same color
+            {
+                a.PieceColor = null; // Piece A color is deleted
+            }
+        }
+
+    }
 
 }
 
